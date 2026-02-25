84ba26b3cdd3051a4d53e2b263e89e6d5a922e01
mikedomingues <miguelsilvadomingues@gmail.com>
2026-02-25 20:48:18 +0000

    Update 3_Tarefas.py

--- pages/3_Tarefas.py
+++ pages/3_Tarefas.py
@@ -1,1 +1,14 @@
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
 
+st.set_page_config(page_title="Tarefas", page_icon="✅")
+
+if "logged_in" not in st.session_state or not st.session_state.logged_in:
+    st.warning("⚠️ Por favor, faça login na página principal.")
+    st.stop()
+
+conn = st.connection("gsheets", type=GSheetsConnection)
+st.title("✅ Tarefas da Família")
+
+df_tarefas = conn.read(worksheet="tarefas", ttl=0)
+st.table(df_tarefas)
